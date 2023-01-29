5ff6112364fc8fdcbb2b610ec81f4008fb8fce9d
TechnologyTim <techtim02@gmail.com>
2023-01-29 16:12:12 +0100

    Added theme files

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
 # My personal PowerShell config using oh my posh
 
 # oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/honukai.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:\Users\timwa\Documents\WindowsPowerShell\honukai.omp.json' | Invoke-Expression
